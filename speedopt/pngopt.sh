9f383192398df11a65e574a60703f5a196ca0bbb
Wing Ying Chow <wingying.chow@gmail.com>
2018-10-06 01:21:00 +0200

    added bash script to optimize pngs

---
+++ speedopt/pngopt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+imgdir=/opt/indico/.venv/lib/python2.7/site-packages/indico/htdocs/images
+
+if [[ -d ${imgdir}-bak ]]; then
+ echo "Backup exists"
+else
+ rsync -avn ${imgdir} ${imgdir}-bak
+fi
+
+echo "Before pngquant"
+du -bsh ${imgdir}-bak
+du -bsh ${imgdir} 
+
+if [[ -f ${imgdir}/pngquanted ]]; then
+ echo "PNGs already optimized"
+ exit
+else
+ find ${imgdir} -name \*.png | while read line; do pngquant -f --ext .png $line; done
+ touch ${imgdir}/pngquanted
+ touch ${imgdir}/pngquanted-on-`date +'%Y%m%d'`
+fi
+
+echo "After pngquant"
+du -bsh ${imgdir}-bak
+du -bsh ${imgdir} 
+
+exit
